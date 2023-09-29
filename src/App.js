df44ad43348751a5d655821913e6fe7f21bc13af
ja.bernalg1 <ja.bernalg1@uniandes.edu.co>
2023-09-29 02:03:58 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <button className="btn btn-primary">Botón de Bootstrap</button>
+    </div>
+  );
+}
+
+export default App;
+
+
+
